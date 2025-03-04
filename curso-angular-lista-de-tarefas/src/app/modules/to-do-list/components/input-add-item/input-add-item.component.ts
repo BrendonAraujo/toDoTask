35676137add831ec5fef7af49ffced5eaa5c0641
BrendonAraujo <brendong.araujo@gmail.com>
2025-03-04 13:56:24 -0300

    #ADD - Itens on storage

--- curso-angular-lista-de-tarefas/src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ curso-angular-lista-de-tarefas/src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,10 +1,14 @@
-import {ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Output, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild} from '@angular/core';
 import {IListItem} from '../../Interface/IListItem.interface';
+import {JsonPipe, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe,
+    NgClass
+  ],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
@@ -13,6 +17,7 @@ export class InputAddItemComponent {
 
   @ViewChild("inputText") public inputText!: ElementRef;
 
+  @Input({required: true}) public  inputListItens: Array<IListItem> = [];
   @Output() public outPutListItem = new EventEmitter<IListItem>();
 
   public focsAndAddItem(value: string){
@@ -27,8 +32,7 @@ export class InputAddItemComponent {
         id,
         checked: false,
         value
-      })
-      console.log("aa")
+      });
       return this.inputText.nativeElement.focus();
     }
   }
--- curso-angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
+++ curso-angular-lista-de-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,13 +1,32 @@
 import {Component, signal} from '@angular/core';
 import {InputAddItemComponent} from '../../components/input-add-item/input-add-item.component';
+import {IListItem} from '../../Interface/IListItem.interface';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [InputAddItemComponent , JsonPipe],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
   public addItem = signal(true)
+
+  private setListItens = signal<IListItem[]>(this.parseItems());
+  public getListItens = this.setListItens.asReadonly();
+
+  private parseItems() {
+    return JSON.parse(localStorage.getItem('@my-list') || '[]')
+  }
+
+  public getInputAndAddItem(item: IListItem) {
+    console.log(item);
+    localStorage.setItem(
+      '@my-list',
+      JSON.stringify([...this.setListItens(), item])
+    );
+
+    return this.setListItens.set(this.parseItems());
+  }
 }
