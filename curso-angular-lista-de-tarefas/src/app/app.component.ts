99ae9cca012c0dd9421a270fcb4f258d62e5acbb
BrendonAraujo <brendong.araujo@gmail.com>
2025-03-04 09:35:05 -0300

    #ADD - First Commit

---
+++ curso-angular-lista-de-tarefas/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `1<router-outlet />2`,
+})
+export class AppComponent {
+}
